30be11956e856d3f34727033d657e167a39e7cf0
AntaNdiaye <76149825+AntaNdiaye@users.noreply.github.com>
2021-07-02 11:02:16 -0400

    Add files via upload

---
+++ careerAPI/src/main/java/com/career/careerAPI/CareerApiApplication.java
@@ -0,0 +1,13 @@
+package com.career.careerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerApiApplication.class, args);
+	}
+
+}
